d6d1b9dfd43022598a404e82210b6127e7efc3c6
grgsiberia <nanashi4129@gmail.com>
2019-02-24 14:50:36 +0900

    能力値クラスを作成

---
+++ SampleRPG/SampleRPG/AbilityScore.cpp
@@ -0,0 +1,17 @@
+﻿#include "AbilityScore.h"
+using namespace rpg;
+
+AbilityScore::AbilityScore()
+	: score(0.f), modify(0.f), vary(0.f)
+{
+}
+
+
+AbilityScore::~AbilityScore()
+{
+}
+
+const int rpg::AbilityScore::GetValue() const
+{
+	return score + vary + modify;
+}
---
+++ SampleRPG/SampleRPG/AbilityScore.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace rpg
+{
+	/**
+	* 能力値
+	*/
+	class AbilityScore
+	{
+		float score;	//! 能力値
+		float modify;	//! 修正値
+		float vary;		//! 増減値
+
+	public:
+		AbilityScore();
+		virtual ~AbilityScore();
+
+		/**
+		* 正味の能力値を得る
+		* @return 正味の能力値
+		*/
+		const int GetValue() const;
+	};
+}
