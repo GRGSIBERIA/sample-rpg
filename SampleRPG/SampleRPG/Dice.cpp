f2eae77d66216b8a2f3335991ee41c7f33185ad3
grgsiberia <nanashi4129@gmail.com>
2019-02-24 14:50:26 +0900

    ダイスを投げるクラスを作成

---
+++ SampleRPG/SampleRPG/Dice.cpp
@@ -0,0 +1,46 @@
+﻿#include "Dice.h"
+using namespace rpg::dice;
+
+rpg::dice::Dice::Dice(const int & numFace)
+	: numFace(numFace), score(0)
+{
+
+}
+
+const int rpg::dice::Dice::Roll()
+{
+	std::uniform_int_distribution<> randomDist(1, numFace);
+	score = randomDist(mt);
+	return std::lroundf(score);
+}
+
+const bool rpg::dice::Dice::IsRolled() const
+{
+	return score > 0;
+}
+
+Dice::~Dice()
+{
+}
+
+rpg::dice::MultiDice::MultiDice(const int & numDice, const int & numFace)
+	: numDice(numDice), numFace(numFace)
+{
+	dices = std::vector<Dice>(numDice);
+}
+
+rpg::dice::MultiDice::~MultiDice()
+{
+}
+
+const int rpg::dice::MultiDice::RollAll()
+{
+	int total = 0;
+
+	for each (auto& dice in dices)
+	{
+		total += dice.Roll();
+	}
+
+	return total;
+}
---
+++ SampleRPG/SampleRPG/Dice.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include <random>
+
+namespace rpg::dice
+{
+	/**
+	* サイコロクラス
+	*/
+	class Dice
+	{
+		const int numFace;
+		int score;
+
+		static std::random_device rnd;	//! 乱数生成
+		static std::mt19937 mt;			//! メルセンヌ・ツイスタを使用する
+
+	public:
+		/**
+		* ダイスインスタンス
+		* @param numFace ダイスの目の数
+		*/
+		Dice(const int& numFace);
+		virtual ~Dice();
+		
+		/**
+		* ダイスを振る
+		* @return ダイスの目
+		*/
+		const int Roll();
+
+		/**
+		* ダイスを振ったかどうか？
+		* @return 振ったならtrue
+		*/
+		const bool IsRolled() const;
+	};
+
+	/**
+	* 複数ダイスを投げるクラス
+	*/
+	class MultiDice
+	{
+		const int numDice;			//! ダイスの個数
+		const int numFace;			//! ダイスの面数
+		std::vector<Dice> dices;	//! ダイスインスタンス
+
+	public:
+		/**
+		* 複数ダイスを管理するクラス
+		* @param numDice ダイスの個数
+		* @param numFace ダイスの面数
+		*/
+		MultiDice(const int& numDice, const int& numFace);
+		~MultiDice();
+
+		/**
+		* すべてのダイスを投げる
+		*/
+		const int RollAll();
+	};
+
+	std::random_device Dice::rnd = std::random_device();
+	std::mt19937 Dice::mt = std::mt19937(Dice::rnd());
+}
